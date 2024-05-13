232cf4d4a550a0a2f407fc73ee5e3236486e872c
Pablo León <pablolv.8.plv@gmail.com>
2024-05-13 18:17:51 +0200

    InformeControllerIT ya hecho

--- Practica6/src/test/java/com/uma/example/springuma/integration/InformeControllerIT.java
+++ Practica6/src/test/java/com/uma/example/springuma/integration/InformeControllerIT.java
@@ -5,8 +5,16 @@ import static org.hamcrest.Matchers.hasSize;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
+import java.util.Calendar;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.uma.example.springuma.controller.InformeController;
+import com.uma.example.springuma.model.Imagen;
+import com.uma.example.springuma.model.Medico;
+import com.uma.example.springuma.model.Paciente;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -24,88 +32,136 @@ import jakarta.annotation.PostConstruct;
 import reactor.core.publisher.Mono;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class PersonaControllerWebTestClientIT {
-/* 
+class InformeControllerIT {
+
     @LocalServerPort
     private Integer port;
 
     private WebTestClient client;
 
-    private Persona persona;
+    private Imagen imagen;
+    private Paciente paciente;
+    private Medico medico;
+    private Informe informe;
+    ObjectMapper objectMapper = new ObjectMapper();
 
     // After dependency injection
     @PostConstruct
     public void init() {
-        client = WebTestClient.bindToServer().baseUrl("http://localhost:"+port)
-        .responseTimeout(Duration.ofMillis(30000)).build();
+        client = WebTestClient.bindToServer().baseUrl("http://localhost:" + port)
+                .responseTimeout(Duration.ofMillis(30000)).build();
 
-        persona = new Persona();
-        persona.setNombre("Alumno"); 
-        persona.setDni("122");
-        persona.setEdad(16);
-        persona.setId(1);
+        medico = new Medico("11111111A", "Mari", "trauma");
+        medico.setId(1);
+
+        paciente = new Paciente("Rosa", 30, "trauma", "84392821B", medico);
+        paciente.setId(1);
+
+        imagen = new Imagen();
+        imagen.setId(1);
+        imagen.setNombre("img");
+        imagen.setPaciente(paciente);
+        imagen.setFecha(Calendar.getInstance());
+        imagen.setFile_content(null);
+
+        informe = new Informe();
+        informe.setId(1);
+        informe.setImagen(imagen);
+    }
+
+    public void crearMedicoYPaciente(Medico medico, Paciente paciente) {
+        client.post().uri("/medico")
+                .body(Mono.just(medico), Medico.class)
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody().returnResult();
+
+        client.post().uri("/paciente")
+                .body(Mono.just(paciente), Paciente.class)
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody().returnResult();
     }
 
     @Test
-    @DisplayName("Returns the empty list of personas initially")
-    public void getPersonas_ReturnPersonasEmpty() {
-        client.get().uri("/personas")
-            .accept(MediaType.APPLICATION_JSON)
-            .exchange() // hace la peticion
-            .expectStatus().isOk() // comprueba que el codigo es OK
-            .expectHeader().valueEquals("Content-Type", "application/json") // comprueba que el content type es json
-            .expectBody().jsonPath("$", hasSize(0)); // comprueba que la respuesta tenga un array con tamanyo 0
-    }
+    @DisplayName("Creo un informe correctamente")
+    public void create_informeAllCorrect_returnTrue() throws IOException {
+        crearMedicoYPaciente(medico, paciente);
 
-    @Test
-    @DisplayName("Create a persona and get it correctly")
-    public void createPersonaPost_isObtainedWithGet() {
+        File uploadFile = new File("src/test/resources/healthy.png");
 
-        // crea una persona
-        client.post().uri("/persona")
-            .body(Mono.just(persona), Persona.class)
-            .exchange()
-            .expectStatus().isCreated()
-            .expectBody().returnResult();
-        
-        // obtiene la persona con ID 1
-        FluxExchangeResult<Persona> result = client.get().uri("/persona/1")
-            .exchange()
-            .expectStatus().isOk().returnResult(Persona.class); // comprueba que la respuesta es de tipo persona
-
-        Persona personaObtained = result.getResponseBody().blockFirst(); // Obtiene el objeto persona en concreto
-
-        assertEquals(persona, personaObtained);
-    }
-
-    @Test
-    @DisplayName("Submit a logo to persona/logo controller should return a valid response containing the name of the file")
-	void submitValidLogo_shouldRespondValidResponse() throws Exception {
-
-        // crea una persona
-        client.post().uri("/persona")
-        .body(Mono.just(persona), Persona.class)
-        .exchange()
-        .expectStatus().isCreated()
-        .expectBody().returnResult();
-        
-        File uploadFile = new File("./src/test/resources/uma.png");
-
-        
         MultipartBodyBuilder builder = new MultipartBodyBuilder();
-        builder.part("logo", new FileSystemResource(uploadFile));
+        builder.part("image", new FileSystemResource(uploadFile));
+        builder.part("paciente", paciente);
 
-        // Envia el logo para la persona en concreto 1
         FluxExchangeResult<String> responseBody = client.post()
-        .uri("/persona/1/logo")
-        .contentType(MediaType.MULTIPART_FORM_DATA)
-        .body(BodyInserters.fromMultipartData(builder.build()))
-        .exchange()
-        .expectStatus().is2xxSuccessful().returnResult(String.class);
-        
-        String result = responseBody.getResponseBody().blockFirst();
+                .uri("/imagen")
+                .contentType(MediaType.MULTIPART_FORM_DATA)
+                .body(BodyInserters.fromMultipartData(builder.build()))
+                .exchange()
+                .expectStatus().is2xxSuccessful().returnResult(String.class);
 
-        assertEquals("Received uma.png", result);
+
+        FluxExchangeResult<String> prediction = client.get()
+                .uri("/imagen/predict/1")
+                .exchange()
+                .expectStatus().is2xxSuccessful().returnResult(String.class);
+
+        String result = prediction.getResponseBody().blockFirst();
+        JsonNode jsonNode = objectMapper.readTree(prediction.getResponseBodyContent());
+        String pre = jsonNode.get("prediction").asText();
+
+        informe.setPrediccion(pre);
+        informe.setContenido("No se");
+
+        client.post().uri("/informe")
+                .body(Mono.just(informe), Informe.class)
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody().returnResult();
     }
-    */
+
+
+    @Test
+    @DisplayName("Elimino un informe correctamente")
+    public void delente_informeAllCorrect_returnTrue() throws IOException {
+        crearMedicoYPaciente(medico, paciente);
+
+        File uploadFile = new File("src/test/resources/healthy.png");
+
+        MultipartBodyBuilder builder = new MultipartBodyBuilder();
+        builder.part("image", new FileSystemResource(uploadFile));
+        builder.part("paciente", paciente);
+
+        FluxExchangeResult<String> responseBody = client.post()
+                .uri("/imagen")
+                .contentType(MediaType.MULTIPART_FORM_DATA)
+                .body(BodyInserters.fromMultipartData(builder.build()))
+                .exchange()
+                .expectStatus().is2xxSuccessful().returnResult(String.class);
+
+
+        FluxExchangeResult<String> prediction = client.get()
+                .uri("/imagen/predict/1")
+                .exchange()
+                .expectStatus().is2xxSuccessful().returnResult(String.class);
+
+        String result = prediction.getResponseBody().blockFirst();
+        JsonNode jsonNode = objectMapper.readTree(prediction.getResponseBodyContent());
+        String pre = jsonNode.get("prediction").asText();
+
+        informe.setPrediccion(pre);
+        informe.setContenido("No se");
+
+        client.post().uri("/informe")
+                .body(Mono.just(informe), Informe.class)
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody().returnResult();
+
+        client.delete().uri("/informe/1")
+                .exchange()
+                .expectStatus().is2xxSuccessful();
+    }
+
 }
